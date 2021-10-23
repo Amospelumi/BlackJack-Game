1e13493320bb544b108b4e8127e851629ce480d3
Amospelumi <oluwapelumiabiodun45@gmail.com>
2021-10-23 13:26:15 +0100

    update

--- index.js
+++ index.js
@@ -1,39 +1,52 @@
-let firstCard
-let secondCard
-let sum
-let hasBlackJack = false
-let isAlive = true
+let firstCard = Math.floor(Math.random()*6)
+let secondCard = Math.floor(Math.random()*6)
+let cards = [firstCard, secondCard]
 let message = ""
 
-// function for the start button
+// Cards Addition
+let sum = cards[0] + cards[1]
+
 function startGame(){
-    firstCard = parseInt(prompt("Enter a card number: "))
-    secondCard = parseInt(prompt("Enter a card number: "))
-    sum = firstCard + secondCard
+    renderGame()
+}
+
+// function for the start button
+function renderGame(){
+    
+    let cardEL = document.querySelector('#cardEl').textContent = "Cards " 
+
+    for(let i = 0; i < cards.length; i++){
+        cardEL = document.querySelector('#cardEl').textContent += cards[i] + " "
+    }
+
+
     // document.getElementById('sumEl').textContent = "Sum: " + sum
     document.querySelector('#sumEl').textContent = "Sum: " + sum
 
-    // Cards result
-    document.querySelector('#cardEl').textContent = "Cards " + firstCard + ", " + secondCard
     
     if(sum <= 20){
         message = "Do you want to draw another card?"
         document.getElementById('result').textContent = message
+
     }else if (sum === 21){
         message = "Whooh, You've got a BlackJack"
         document.getElementById('result').textContent = message
-        hasBlackJack = true
+
     }else{
         message = "You are out of the game"
         document.getElementById('result').textContent = message
-        isAlive = false
     }
 }
 
 
-function newCard(){
-    NewCard = parseInt(prompt("Enter a card number: "))
-    sum += NewCard
+function newCARD(){
+    let anotherCard = Math.floor(Math.random()*6)
 
-    startGame()
+    sum += anotherCard
+    cards.push(anotherCard)
+    console.log(cards)
+
+    document.getElementById('sumEl').textContent = sum
+
+    renderGame()
 }
